c52d74c4c951936f93cf4820beb7a33e570df12c
Omanshuaman <omanshuaman@gmail.com>
2025-04-24 17:08:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { Background } from "./components/Background";
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 1, 5], fov: 75 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import { OrbitControls, Float } from "@react-three/drei";
+import { Background } from "./Background";
+import { Jet } from "./Jet";
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <Background />
+      <Float floatIntensity={1.5} rotationIntensity={0.5} speed={2}>
+        <Jet scale={0.4} rotation-y={Math.PI * 1} rotation-x={Math.PI * 0.01} />
+      </Float>
+    </>
+  );
+};
