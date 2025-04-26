d4756acb44b158af7b94838d277171ba42eb5336
Omanshuaman <omanshuaman@gmail.com>
2025-04-26 15:45:52 +0530

    Refactor CloudsGroup to enhance cloud positioning controls and integrate Background component

---
+++ src/components/Background.jsx
@@ -0,0 +1,28 @@
+import { Environment, Sphere } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Gradient, LayerMaterial } from "lamina";
+import { useRef } from "react";
+
+import * as THREE from "three";
+
+export const Background = () => {
+  return (
+    <>
+      <Environment preset="sunset" />
+      <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
+        <LayerMaterial
+          lighting="physical"
+          transmission={1}
+          side={THREE.BackSide}>
+          <Gradient
+            colorB={"#357ca1"}
+            colorA={"white"}
+            axes={"y"}
+            start={-1.2}
+            end={0.2}
+          />
+        </LayerMaterial>
+      </Sphere>
+    </>
+  );
+};
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,18 +4,20 @@ import { Sky } from "@react-three/drei";
 
 import CloudsGroup from "./Clouds";
 import { Perf } from "r3f-perf";
+import { Background } from "./Background";
 export const Experience = () => {
   return (
     <>
       <Perf position="top-left" />
       <CameraControls />
-      <Sky />
+      {/* <Sky /> */}
 
       <ambientLight intensity={Math.PI / 3} />
 
       <Float floatIntensity={2} rotationIntensity={0} speed={2}>
         <Jet scale={0.4} rotation-y={Math.PI * 1} rotation-x={Math.PI * 0.01} />
       </Float>
+      <Background />
 
       <CloudsGroup />
     </>
